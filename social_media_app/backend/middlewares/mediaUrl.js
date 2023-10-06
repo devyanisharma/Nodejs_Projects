41199f5b0d30e89cee7d8ed972fe2fb7f61c954c
devyanisharma <46523246+devyanisharma@users.noreply.github.com>
2023-10-06 18:46:14 +0530

    Update mediaUrl.js

--- social_media_app/backend/middlewares/mediaUrl.js
+++ social_media_app/backend/middlewares/mediaUrl.js
@@ -2,14 +2,7 @@ const { S3Client, GetObjectCommand, PutObjectCommand ,ListObjectsV2Command} = re
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner")
 
 //it will call on behalf of node.js
-const s3Client = new S3Client({
-    region: "ap-south-1",
-    credentials: {
-        accessKeyId: "AKIA6FNBXNUNTTL6NBUX",
-        secretAccessKey: "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"
 
-    }
-})
 const getsignedurl = function (req,res,next){
     const file = req.file;
     async function getObjectURL(key) {
