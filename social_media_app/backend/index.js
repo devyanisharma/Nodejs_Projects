e822416dc41c6e6fd617bf3da13c84ac3ac215cc
devyanisharma <46523246+devyanisharma@users.noreply.github.com>
2023-10-06 18:45:26 +0530

    Update index.js

--- social_media_app/backend/index.js
+++ social_media_app/backend/index.js
@@ -14,69 +14,6 @@ const swaggerUi = require("swagger-ui-express");
 const { S3Client, GetObjectCommand, PutObjectCommand ,ListObjectsV2Command} = require('@aws-sdk/client-s3')
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner")
 
-//it will call on behalf of node.js
-// const s3Client = new S3Client({
-//     region: "ap-south-1",
-//     credentials: {
-//         accessKeyId: "AKIA6FNBXNUNTTL6NBUX",
-//         secretAccessKey: "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"
-
-//     }
-// })
-
-// async function getObjectURL(key) {
-//     const command = new GetObjectCommand({
-//         Bucket: 'social-media-post',
-//         Key: key
-
-//     })
-//     const url = await getSignedUrl(s3Client, command)
-//     return url;
-// }
-
-// getObjectURL('download (2).jpeg').then((data) => {
-//     console.log("get url for icecream img", data)
-// }).catch((error) => {
-//     console.log(error)
-// })
-
-// async function putObject(filename, contentType) {
-//     const command = new PutObjectCommand({
-//         Bucket: 'social-media-post',
-//         Key: `/uploads/user-uploads/${filename}`,
-//         ContentType: contentType
-
-//     })
-//     const url = await getSignedUrl(s3Client, command)
-//     return url;
-// }
-
-// putObject('uploadedfile.jpeg',"image/jpeg").then((data)=>{
-//     console.log(`signed url for put ${data}`)
-//     return data;
-// }).catch((error)=>{
-//     console.log(error)
-// })
-// getObjectURL("/uploads/user-uploads/uploadedfile.jpeg").then((data) => {
-//     console.log("get signed url for new uploaded", data)
-// }).catch((error) => {
-//     console.log(error)
-// })
-
-// //to get list details of all uploads
-// async function listObjects(){
-//     const command = new ListObjectsV2Command({
-//         Bucket: 'social-media-post',
-//         Key:"/"
-//     })
-//     const result =  await s3Client.send(command)
-//     console.log(result)
-// }
-// async function init(){await listObjects();}
-// init();
-
-
-
 app.use(express.json()); // parses incoming requests with JSON payloads 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
